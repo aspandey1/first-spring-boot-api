932cb76f64564db0318aaefbf08232dc0f86e086
Ashish Pandey <ashish.apandey@hotmail.com>
2023-12-21 19:33:26 -0600

    Added Validation Dependency

--- src/main/java/com/ashish/contentcalander/controller/ContentController.java
+++ src/main/java/com/ashish/contentcalander/controller/ContentController.java
@@ -2,16 +2,17 @@ package com.ashish.contentcalander.controller;
 
 import com.ashish.contentcalander.model.Content;
 import com.ashish.contentcalander.repository.ContentCollectionRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/content")
+@CrossOrigin()
 public class ContentController {
 
     private final ContentCollectionRepository repository;
@@ -34,13 +35,13 @@ public class ContentController {
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("")
-    public void create(@RequestBody Content content){
+    public void create(@Valid @RequestBody Content content){
         repository.save(content);
     }
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/{id}")
-    public void update(@RequestBody Content content, @PathVariable Integer id){
+    public void update(@Valid @RequestBody Content content, @PathVariable Integer id){
         if(!repository.existsById(id)){
            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Content not found");
         }
